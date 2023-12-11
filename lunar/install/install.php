5002d36a78b59686b350cae6c654c279d8cb1176
lucionescu <lucian.ionesq@gmail.com>
2023-12-11 15:59:27 +0200

    Added mobilepay method

---
+++ lunar/install/install.php
@@ -0,0 +1,111 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+
+
+use Joomla\CMS\Factory;
+
+/**
+ * 
+ */
+class plgVmPaymentLunarInstallerScript
+{
+	private $db;
+	private $mobilePayName;
+
+	public function __construct()
+	{
+		$this->db = Factory::getDbo();
+		$this->mobilePayName = 'lunar_mobilepay';
+	}
+
+	public function uninstall($parent){}
+	public function postflight($type, $parent){}
+
+	public function preflight($type, $parent)
+	{
+		if (!class_exists( 'VmConfig' )) {
+			require(JPATH_ROOT .'/administrator/components/com_virtuemart/helpers/config.php');
+		}
+		
+		VmConfig::loadConfig();	
+	}
+
+	public function install($parent)
+	{
+		if(!class_exists('GenericTableUpdater')) {
+			require(VMPATH_ADMIN .'/helpers/tableupdater.php');
+		}
+
+		$updater = new GenericTableUpdater();
+
+		$updater->updateMyVmTables(dirname(__FILE__) .'/install.sql');
+
+		$this->maybeInstallMobilePayMethod();
+	}
+
+	/** */
+	private function maybeInstallMobilePayMethod()
+	{
+		$methodInstalled = $this->db->setQuery(
+				$this->db->getQuery(true)
+					->select('COUNT(*)')
+					->from('#__extensions')
+					->where('element = ' . $this->db->quote($this->mobilePayName))
+			)->loadResult();
+
+		$manifestFile = JPATH_VM_PLUGINS . DS . 'vmpayment' . DS . $this->mobilePayName . DS . $this->mobilePayName . '.xml';
+		$manifest = simplexml_load_file($manifestFile);
+
+		$manifestData = [
+			'name' => (string) $manifest->name,
+			'type' => 'plugin',
+			'description' => (string) $manifest->description,
+			'creationDate' => (string) $manifest->creationDate,
+			'author' => (string) $manifest->author,
+			'copyright' => (string) $manifest->copyright,
+			'authorEmail' => (string) $manifest->authorEmail,
+			'authorUrl' => (string) $manifest->authorUrl,
+			'group' => '',
+			'filename' => $this->mobilePayName,
+
+		];
+
+		if ((int) $methodInstalled > 0) {
+			$this->updateMobilePayMethod($manifestData);
+
+		} else {
+			$this->insertMobilePayMethod($manifestData);
+		}
+	}
+
+	/** */
+	private function updateMobilePayMethod($manifestData)
+	{
+		// $this->db->setQuery("UPDATE `#__extensions` SET `manifest_cache`='" 
+		// 	. json_encode($manifestData) . "' WHERE `element`='" . $this->mobilePayName . "'")->execute();
+
+		$this->db->getQuery(true)
+			->update('#__extensions')
+			->set(['manifest_cache = ' . $this->db->quote(json_encode($manifestData))])
+			->where('element=' . $this->db->quote($this->mobilePayName));
+	}
+
+	/** */
+	private function insertMobilePayMethod($manifestData)
+	{
+		$this->db->getQuery(true)
+			->insert('#__extensions')
+			->set([
+				'name = ' . $this->db->quote($manifestData['name']), 
+				'type = ' . $this->db->quote('plugin'), 
+				'element = ' . $this->db->quote($this->mobilePayName), 
+				'folder = ' . $this->db->quote('vmpayment'), 
+				'client_id = 0', 
+				'manifest_cache = ' . $this->db->quote(json_encode($manifestData)), 
+				'params = ' . $this->db->quote('{}'), 
+				'custom_data = ""', 
+				'system_data = ""',
+			]);
+	}
+	
+}
---
+++ lunar_mobilepay/tmpl/pay_before.php
@@ -0,0 +1,5 @@
+<?php
+ defined('_JEXEC') or die('Restricted access');
+
+
+include( JPATH_VM_PLUGINS . DS . 'vmpayment' . DS . 'lunar' . DS . 'tmpl' . DS . 'pay_before.php');
