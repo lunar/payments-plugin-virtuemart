0b97dc972a679dfef2e2a8fe88e5f2c0f74598ae
lucionescu <lucian.ionesq@gmail.com>
2023-12-14 19:08:07 +0200

    Removed install separate mobilepay plugin

--- lunar_mobilepay/tmpl/order_done.php
+++
@@ -1,5 +0,0 @@
-<?php
- defined('_JEXEC') or die('Restricted access');
-
-
-include( JPATH_VM_PLUGINS . DS . 'vmpayment' . DS . 'lunar' . DS . 'tmpl' . DS . 'order_done.php');
--- lunar_mobilepay/tmpl/pay_before.php
+++
@@ -1,5 +0,0 @@
-<?php
- defined('_JEXEC') or die('Restricted access');
-
-
-include( JPATH_VM_PLUGINS . DS . 'vmpayment' . DS . 'lunar' . DS . 'tmpl' . DS . 'pay_before.php');
